5830872b905ba2173612103a9ae864b0e9a4b0bf
Erkki Tikk <erkki.tikk@gmail.com>
2022-10-27 15:50:32 +0300

    cv done

--- profilepage/src/components/Cv.js
+++ profilepage/src/components/Cv.js
@@ -25,14 +25,18 @@ export default function Cv() {
             <td>Tartu, Estonia</td>
           </tr>
           <tr>
-            <td>e-mail:</td>
+            <td>E-mail:</td>
             <td>
               <a href="mailto:erkkitikk@gmail.com">erkkitikk@gmail.com</a>
             </td>
           </tr>
           <tr>
-            <td>tel:</td>
+            <td>Tel:</td>
             <td>+372 511 7441</td>
+          </tr>
+          <tr>
+            <td>Discord:</td>
+            <td>Erkki Tikk#8188</td>
           </tr>
           <tr>
             <td>Linkedin:</td>
@@ -94,27 +98,91 @@ export default function Cv() {
       </table>
       <br />
 
-      <h2>Certifications</h2>
+      <h2>Certifications and training</h2>
       <table>
         <tbody>
           <tr>
-            <td>2018 - Present</td>
-            <td>Asymmetric Systems OÜ, military architect and inventor</td>
+            <td>Tech stack</td>
+            <td>JS, TS, React, Sqlite, Go, Rust</td>
           </tr>
           <tr>
-            <td>2017 - 2021</td>
-            <td>Estonian Centre for Defence Investment, chief architect</td>
+            <td>Other programs</td>
+            <td>Autodesk: AutoCad, Revit, Inventor; Photoshop, Illustrator</td>
           </tr>
           <tr>
-            <td>2010 - 2017</td>
-            <td>Estonian Defence Forces, chief architect</td>
+            <td>2022</td>
+            <td>Awarded military rank: Second Liutenant (OF-1)</td>
           </tr>
           <tr>
-            <td>2007 - 2010</td>
-            <td>Osaühing Sirkel&Mall, architect</td>
+            <td>2021 - Present</td>
+            <td>Active member of Estonian Defence League</td>
+          </tr>
+          <tr>
+            <td>2010 - 2021</td>
+            <td>EST / NATO / EU Secret level information certified </td>
+          </tr>
+          <tr>
+            <td>2018</td>
+            <td>Garage48 Defence Makeathon prototyping team leader</td>
+          </tr>
+          <tr>
+            <td>2017</td>
+            <td>Concealed carry gun permit</td>
+          </tr>
+          <tr>
+            <td>2014</td>
+            <td>EDF security officers training</td>
+          </tr>
+          <tr>
+            <td>2011</td>
+            <td>EDF reserve officers training, ensign (OF D)</td>
+          </tr>
+          <tr>
+            <td>2002 - 2003</td>
+            <td>Estonian Defence Forces, conscript service</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>Kuperjanov battalion, 120 mm mortar battery surveyor, corporal (OR 2)</td>
+          </tr>
+          <tr>
+            <td>2001</td>
+            <td>B-category driver's licence</td>
+          </tr>
+          <tr>
+            <td>2000</td>
+            <td>PADI Open Water Diver licence</td>
           </tr>
         </tbody>
       </table>
+      <br />
+
+      <h2>Additional info</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td>Interests</td>
+            <td>astrophysics, quantum physics, AI, computer tehnology, robotics,</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>military technology, tanks, firearms, foreign politics</td>
+          </tr>
+          <tr>
+            <td>Hobbies</td>
+            <td>Dungeons&Dragons, airsoft, pc gaming, inventing</td>
+          </tr>
+          <tr>
+            <td>Languages</td>
+            <td>Estonian, English, German</td>
+          </tr>
+          <tr>
+            <td>Other</td>
+            <td>Married, two daughters</td>
+          </tr>
+        </tbody>
+      </table>
+      <br />
       <br />
     </div>
   );
--- profilepage/src/components/Projects.js
+++ profilepage/src/components/Projects.js
@@ -8,7 +8,9 @@ export default function Projects() {
         <h1>Bomberman clone</h1>
         <a href="https://tikk-bomberman.netlify.app/src/">Link to hosted project</a>
         <br />
-        <a href="https://tikk-bomberman.netlify.app/src/">Link to GitHub repository</a>
+        <a href="https://github.com/etikk/career-profile/tree/main/bomberman">
+          Link to GitHub repository
+        </a>
         <h3>Description</h3>
         <p>
           This game was created for the kood/Jõhvi div-01 task "make-your-game". The aim
@@ -27,8 +29,8 @@ export default function Projects() {
       </div>
       <div>
         <h1>Real-time forum</h1>
-        <a href="https://tikk-bomberman.netlify.app/src/">Link to hosted project</a>
-        <br />
+        {/* <a href="https://tikk-bomberman.netlify.app/src/">Link to hosted project</a>
+        <br /> */}
         <a href="https://tikk-bomberman.netlify.app/src/">Link to GitHub repository</a>
         <h3>Description</h3>
         <p>
@@ -48,8 +50,8 @@ export default function Projects() {
       </div>
       <div>
         <h1>Road intersection</h1>
-        <a href="https://tikk-bomberman.netlify.app/src/">Link to hosted project</a>
-        <br />
+        {/* <a href="https://tikk-bomberman.netlify.app/src/">Link to hosted project</a>
+        <br /> */}
         <a href="https://tikk-bomberman.netlify.app/src/">Link to GitHub repository</a>
         <h3>Description</h3>
         <p>
@@ -69,24 +71,24 @@ export default function Projects() {
       </div>
       <div>
         <h1>Profile page</h1>
-        <a href="https://tikk-bomberman.netlify.app/src/">Link to hosted project</a>
+        <a href="https://tikk-profile.netlify.app/">Link to hosted project</a>
         <br />
-        <a href="https://tikk-bomberman.netlify.app/src/">Link to GitHub repository</a>
+        <a href="https://github.com/etikk/career-profile/tree/main/profilepage">
+          Link to GitHub repository
+        </a>
         <h3>Description</h3>
         <p>
-          This game was created for the kood/Jõhvi div-01 task "make-your-game". The aim
-          of the task was to create a clone of a classic, with Javascript and CSS and
-          without using the canvas object or any external packages. The task focused on
-          DOM manipulation, using RequestAnimationFrame and maintaining a steady 60fps at
-          all times. To get a steady playing experience on different monitors, frame
-          throttling was added. The aim of the game is to move around and blow up enemies
-          with bombs. The game is played with the arrow keys and the spacebar. The game
-          must be able to be stopped and started again without significant frame drops.
-          The most difficult part was writing collision detection and smooth movement
-          around corners. For smooth animation, a sprite image containing all different
-          movements is moved with CSS under a virtual hole in the given layer.{" "}
+          This sample project is the very same profile-page that you are currently
+          visiting. We had a kood/Jõhvi div-01 task called "graphql" that was composed of
+          making graphql queries into the school server API and presenting the data on a
+          profile page. The graphs and data in the sections "Profile", "Div-01 tasks" and
+          "Levels by type" represent this task and are making the respected queries. I
+          decided to build upon this project and turn this into my actual developer
+          profile page that you are currently visiting. The page has a simple React
+          structure with most data hardcoded into html. The interactive graphs were made
+          with react-chart-js.{" "}
         </p>
-        <p>Tech stack: Vanilla JS, CSS</p>
+        <p>Tech stack: JS, React, CSS, Graphql, react-chart-js</p>
       </div>
       <br />
       <br />
