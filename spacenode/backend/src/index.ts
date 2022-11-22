53f10ab85273f3b29ad2e72af17d07e001212969
Erkki Tikk <erkki.tikk@gmail.com>
2022-11-22 15:44:00 +0200

    basic backend running

--- spacenode/backend/src/index.ts
+++ spacenode/backend/src/index.ts
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req: any, res: { send: (arg0: string) => void }) => {
   res.send("Express + TypeScript Server");
@@ -13,3 +13,10 @@ app.get("/", (req: any, res: { send: (arg0: string) => void }) => {
 app.listen(port, () => {
   console.log(`[server]: Server is running at https://localhost:${port}`);
 });
+
+//load json data from assets/example.json
+const data = require("./assets/example.json");
+
+console.log(data.validUntil);
+console.log(data.legs.length);
+console.table(data.legs);
